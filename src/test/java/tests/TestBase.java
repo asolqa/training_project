91c9fe3bf6146cda0bac9d1f2c1bc8193d3c7159
Anna Solyanskaya <ASolyanskaya@it-one.ru>
2024-04-29 15:13:09 +0300

    Added remote data

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -12,8 +12,6 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
 
-import static com.codeborne.selenide.Selenide.open;
-
 
 public class TestBase {
 
@@ -21,18 +19,18 @@ public class TestBase {
     @Step("Open https://askmask.ru")
     static void setUpConfig() {
 
-        Configuration.browserSize = "1920x1080";
+        /*Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
         Configuration.baseUrl = "https://askmask.ru";
         open("https://askmask.ru");
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";*/
 
-        /*Configuration.browser = System.getProperty("browser");
+        Configuration.browser = System.getProperty("browser");
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
         Configuration.browserVersion = System.getProperty("browserVersion", "120.0");
         Configuration.pageLoadStrategy = "eager";
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.remote = "https://user1:1234@" + System.getProperty("wdHost", "selenoid.autotests.cloud") + "/wd/hub";*/
+        Configuration.baseUrl = "https://askmask.ru";
+        Configuration.remote = "https://user1:1234@" + System.getProperty("wdHost", "selenoid.autotests.cloud") + "/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
